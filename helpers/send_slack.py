476eec6f83a42cd4a4a3c5136ab3f706e82410fc
mirage <miragelu@seekasia.com>
2019-04-08 09:52:45 +0800

    remove token

--- helpers/send_slack.py
+++ helpers/send_slack.py
@@ -51,8 +51,8 @@ def send_slack(token, channel, msg, attachments):
 
 if __name__ == "__main__":
     now = time.time()
-    token = 'xoxp-9909592998-92079341841-344756143474-7feb9d544f4d69d35e72d4c0cc086ecd'
-    channel = 'CAF8QRX4N' #test-api:CAF8QRX4N
+    token = '****'
+    channel = 'CAF8QRX4N' #rc_monitor: C80G17TM1;     test-api: CAF8QRX4N
     #message demo: https://api.slack.com/docs/messages/builder?msg=%7B%22attachments%22%3A%5B%7B%22fallback%22%3A%22Required%20plain-text%20summary%20of%20the%20attachment.%22%2C%22color%22%3A%22%2336a64f%22%2C%22pretext%22%3A%22Optional%20text%20that%20appears%20above%20the%20attachment%20block%22%2C%22author_name%22%3A%22Bobby%20Tables%22%2C%22author_link%22%3A%22http%3A%2F%2Fflickr.com%2Fbobby%2F%22%2C%22author_icon%22%3A%22http%3A%2F%2Fflickr.com%2Ficons%2Fbobby.jpg%22%2C%22title%22%3A%22Slack%20API%20Documentation%22%2C%22title_link%22%3A%22https%3A%2F%2Fapi.slack.com%2F%22%2C%22text%22%3A%22Optional%20text%20that%20appears%20within%20the%20attachment%22%2C%22fields%22%3A%5B%7B%22title%22%3A%22Priority%22%2C%22value%22%3A%22High%22%2C%22short%22%3Afalse%7D%5D%2C%22image_url%22%3A%22http%3A%2F%2Fmy-website.com%2Fpath%2Fto%2Fimage.jpg%22%2C%22thumb_url%22%3A%22http%3A%2F%2Fexample.com%2Fpath%2Fto%2Fthumb.png%22%2C%22footer%22%3A%22Slack%20API%22%2C%22footer_icon%22%3A%22https%3A%2F%2Fplatform.slack-edge.com%2Fimg%2Fdefault_application_icon.png%22%2C%22ts%22%3A123456789%7D%5D%7D
     attachments = [
         {
